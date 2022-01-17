3c33ccb663e9ef6b0ba3d0ca94e469a4ed0cf79d
Robert Gazyan <b.b.angel.199x@gmail.com>
2022-01-17 14:44:46 +0400

    02. Building Your First  Asp.Net Core API

---
+++ my-books/Data/Services/BooksService.cs
@@ -0,0 +1,79 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM bookVM)
+        {
+            var book = new Book()
+            {
+                Title = bookVM.Title,
+                Description = bookVM.Description,
+                IsRead = bookVM.IsRead,
+                DateRead = bookVM.IsRead ? bookVM.DateRead.Value : null,
+                Rate = bookVM.IsRead ? bookVM.Rate.Value : null,
+                Genre = bookVM.Genre,
+                Author = bookVM.Author,
+                CoverUrl = bookVM.CoverUrl,
+                DateAdded = DateTime.Now
+            };
+
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            var allBooks = _context.Books.ToList();
+            return allBooks;
+        }
+
+        public Book GetBookById(int bookId)
+        {
+            var book = _context.Books.FirstOrDefault(x => x.Id == bookId);
+            return book;
+        }
+
+        public Book UpdateBookById(int bookid, BookVM bookVM)
+        {
+            var book = _context.Books.FirstOrDefault(x => x.Id == bookid);
+            if (book != null)
+            {
+                book.Title = bookVM.Title;
+                book.Description = bookVM.Description;
+                book.IsRead = bookVM.IsRead;
+                book.DateRead = bookVM.IsRead ? bookVM.DateRead.Value : null;
+                book.Rate = bookVM.IsRead ? bookVM.Rate.Value : null;
+                book.Genre = bookVM.Genre;
+                book.Author = bookVM.Author;
+                book.CoverUrl = bookVM.CoverUrl;
+
+                _context.SaveChanges();
+            }
+            return book;
+        }
+
+        public void DeleteBookById(int id)
+        {
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
--- my-books/Startup.cs
+++ my-books/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using my_books.Data;
+using my_books.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,8 @@ namespace my_books
             services.AddControllers();
 
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
+
+            services.AddTransient<BooksService>();
 
             services.AddSwaggerGen(c =>
             {
